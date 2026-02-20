ba629ebfa03ea936bb2b92a2b18e3db81a2064ee
shubham <subhamv2003@gmail.com>
2026-02-20 21:38:21 +0530

    change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/agent", agentRoute);
+app.use("/api/agent/generate", agentRoute);
 
 app.get("/", (req, res) => {
   res.send("AI Gym Agent Running 💪");
